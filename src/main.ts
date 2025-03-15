e381186b3aababcd0b2a0cd89956501ae42dc29e
Usmonqulov-Abduhamid-5018844 <usmonqulovabduhamid00@gmail.com>
2025-03-15 06:20:29 +0500

    ubgred

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { METHODS } from 'http';
+const Port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,8 +21,8 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`Server Started On Post ${process.env.PORT || 3000}`);
+  await app.listen(Port, ()=> {
+    console.log(`Server Started On Post ${Port || 3000}`);
     
   });
 }
